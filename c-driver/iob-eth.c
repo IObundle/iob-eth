3d049f2eec201cf93e4ca75c913fb43db2fc62c1
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2019-04-06 03:36:26 +0100

    fix define

--- c-driver/iob-eth.c
+++ c-driver/iob-eth.c
@@ -7,9 +7,9 @@ void ethInit(unsigned int base)
 {
   // check processor interface
   // write dummy register
-  MEMSET(base, ETHERNET_DUMMY, 0xDEADBEEF);
+  MEMSET(base, ETH_DUMMY, 0xDEADBEEF);
 
   // read and check result
-  if (MEMGET(base, ETHERNET_DUMMY) != 0xDEADBEEF)
+  if (MEMGET(base, ETH_DUMMY) != 0xDEADBEEF)
     uart_puts("Ethernet dummy reg test failed\n");
 }
