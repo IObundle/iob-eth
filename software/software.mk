a03507bcbb47f4bc919f845dfb2bd3855d68230a
P_Miranda <pedro_miranda2@hotmail.com>
2020-07-15 18:18:23 +0100

    Added ETH_RMAC_ADDR define

--- software/software.mk
+++ software/software.mk
@@ -5,3 +5,7 @@ INCLUDE+=-I$(ETHERNET_SW_DIR)
 
 #headers
 HDR+=$(ETHERNET_SW_DIR)/*.h
+
+#define ETH_RMAC_ADDR
+DEFINE+=$(define)ETH_RMAC_ADDR=0x$(shell ethtool -P $(RMAC_INTERFACE) | awk '{print $$3}' | sed "s/://g")
+
