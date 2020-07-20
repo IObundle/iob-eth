83bfdfc14c573427b5b702ebd49f8c7efe3b87ef
P_Miranda <pedro_miranda2@hotmail.com>
2020-07-20 16:00:56 +0100

    Auto add ETHERNET to system
    Created files to automate the addition of ethernet module to system.v in iob-soc

---
+++ hardware/include/pio.v
@@ -0,0 +1,13 @@
+   //ETHERNET
+   output 		     ETH_PHY_RESETN,
+   input 		     PLL_LOCKED,
+
+   input 		     RX_CLK,
+   input [3:0] 		     RX_DATA,
+   input 		     RX_DV,
+
+   input 		     TX_CLK,
+   output 		     TX_EN,
+   output [3:0] 	     TX_DATA,
+
+
