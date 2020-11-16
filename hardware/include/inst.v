c08248ab67ae51c4f0d11207a3a876e6984791e7
pedrompt97 <pedro.m.p.teixeira@tecnico.ulisboa.pt>
2020-11-16 21:30:10 +0000

    Changed interface name to standard native

--- hardware/include/inst.v
+++ hardware/include/inst.v
@@ -7,10 +7,10 @@
       .rst      (reset),
 
       //cpu interface
-      .sel(slaves_req[`valid(`ETHERNET)]),
+      .valid(slaves_req[`valid(`ETHERNET)]),
       .addr(slaves_req[`address(`ETHERNET, `ETH_ADDR_W+2)-2]),
       .data_in(slaves_req[`wdata(`ETHERNET)]),
-      .we(|(slaves_req[`wstrb(`ETHERNET)])),
+      .wstrb(|(slaves_req[`wstrb(`ETHERNET)])),
       .data_out(slaves_resp[`rdata(`ETHERNET)]),
       .ready(slaves_resp[`ready(`ETHERNET)]),
 
