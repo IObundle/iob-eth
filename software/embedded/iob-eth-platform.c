f5323c733a68f162c2bd5b5ca674e39bcdb9a734
P-Miranda <pmiranda.iobundle@gmail.com>
2022-03-09 19:38:56 +0000

    fix(embedded): Add eth_on_transfer_start for emb - Add ethn_on_transfer_start() implementation for embedded version - This function does nothing, but is still needed to keep parity between pc-emul and embedded drivers

---
+++ software/embedded/iob-eth-platform.c
@@ -0,0 +1,7 @@
+#include "iob-eth.h"
+
+void eth_on_transfer_start(void){
+    // this function is needed for pc-emul drivers
+    // so needs to be implemented in embedded drivers to keep parity between
+    // embedded and pc-emul versions
+}
