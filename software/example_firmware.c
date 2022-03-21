e59ae3af536f0b26c287db621901ee21d4a7abb5
P-Miranda <pmiranda.iobundle@gmail.com>
2022-03-21 15:50:30 +0000

    fix(sw): Fix embedded drivers, example_firmware - Fix embedded drivers - Cleanup example_firmware.c - now does not need DDR, exact same code for PC and embedded version on   fpga

--- software/example_firmware.c
+++ software/example_firmware.c
@@ -5,50 +5,32 @@
 #include "iob-eth.h"
 #include "printf.h"
 
-#define TEST_SIZE 2048 // Dont forget to change the value in the makefile as well
-
-#ifndef ADDR_OFFSET
-#define ADDR_OFFSET 0
-#endif
+#define TEST_SIZE 2048
 
 #define PAGE_ALIGN(val) ((val + 0xfff) & ~0xfff)
-
-#ifdef USE_DDR
-  #ifdef PC
-    static char memory[100000];  
-    #define BASE_ADDRESS ((intptr_t) memory)
-  #else
-  #define BASE_ADDRESS (EXTRA_BASE + (1 << (FIRM_ADDR_W)))
-  #endif // PC
-#else
-  static char memory[3000];
-  #define BASE_ADDRESS ((intptr_t) memory)
-#endif // USE_DDR
 
 #ifdef SIM
   #undef ETH_NBYTES
   #define ETH_NBYTES 1024
   #define ADDRESS 2048
+  #define ITERATIONS 1
+  #define TIMES 1
 #endif
-
-#define ITERATIONS 1
-#define TIMES 1
 
 int main()
 {
-  char* buffer;
-  char* recv;
+  /* 1xTEST_SIZE (send data, 1x for send_file(), 1x for send_variable_file())
+   * 1xTEST_SIZE (rcv_file())
+   * 1xTEST_SIZE (rcv_variable_file())
+   */
+  char buffer[3*TEST_SIZE] = {0};
   int i;
-  char str[6000];
-  char errorBuffer[2048];
-  int bufferAddress;
+  int bufferAddress = 0;
 
   uart_init(UART_BASE,FREQ/BAUD);
   eth_init(ETHERNET_BASE);
 
   uart_puts("\nSuccessful Init\n");
-
-  buffer = (char*) BASE_ADDRESS + 4080;
 
   #ifdef SIM
     printf("Size:%d:%x\n",ETH_NBYTES,ETH_NBYTES);
@@ -84,14 +66,14 @@ int main()
         address += ADDRESS + 1;
       }
     }
-  #else
+  #else //ifndef SIM
     printf("\nTesting send,send_variable,rcv and rcv_variable\n");
     printf("In another terminal, do \"make test-eth\" to perform the test\n");
     printf("\"make test-eth\" can also be run before running the firmware\n");
     printf("It should not matter which one is ran first\n");
     printf("For pc-emul, do \"make pc-test-eth\"\n\n");
 
-    bufferAddress = ADDR_OFFSET;    
+    bufferAddress = 0;    
     for(int i = 0; i < TEST_SIZE; i++){
       buffer[bufferAddress + i] = (i % 4) + '0'; // Fill buffer with a repeating pattern of "0123"
     }
@@ -103,18 +85,18 @@ int main()
     eth_send_variable_file(&buffer[bufferAddress],TEST_SIZE);
 
     printf("Testing receive file\n");
-    bufferAddress = ADDR_OFFSET + PAGE_ALIGN(TEST_SIZE * 2);
+    bufferAddress = PAGE_ALIGN(TEST_SIZE);
     eth_rcv_file(&buffer[bufferAddress],TEST_SIZE);
 
     printf("Testing receive variable file\n");
-    bufferAddress = ADDR_OFFSET + PAGE_ALIGN(TEST_SIZE * 4);
+    bufferAddress = PAGE_ALIGN(TEST_SIZE * 2);
     int res = eth_rcv_variable_file(&buffer[bufferAddress]);
 
     if(res != TEST_SIZE){
       printf("Error, the size of the file received is different\n");
       printf("Should be: %d but it is: %d\n",TEST_SIZE,res);
     }
-  #endif
+  #endif //ifdef SIM
 
   uart_puts("\nFinish\n");
   uart_puts("\nWaiting some time to finish\n");
