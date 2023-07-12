9b289fd868982873e6c039980e505ff1badc2da5
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-07-12 04:56:39 +0100

    feat(iob_clkenrst_port*): Use new if_gen.py interface

--- iob_eth.py
+++ iob_eth.py
@@ -6,8 +6,6 @@ from iob_module import iob_module
 
 # Submodules
 from iob_utils import iob_utils
-from iob_clkenrst_portmap import iob_clkenrst_portmap
-from iob_clkenrst_port import iob_clkenrst_port
 from iob_reg import iob_reg
 from iob_reg_e import iob_reg_e
 
@@ -22,9 +20,9 @@ class iob_eth(iob_module):
     def _specific_setup(cls):
         # Hardware headers & modules
         iob_module.generate("iob_s_port")
+        iob_module.generate("clk_en_rst_portmap")
+        iob_module.generate("clk_en_rst_port")
         iob_utils.setup()
-        iob_clkenrst_portmap.setup()
-        iob_clkenrst_port.setup()
         iob_reg.setup()
         iob_reg_e.setup()
 
