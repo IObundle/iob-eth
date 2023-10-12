d7415bd4c71771643a86005dbcfdcbc7b1d0d494
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-10-12 03:35:13 +0100

    fix(extmem): Fix extmem state machines

--- hardware/src/iob_eth_dma.v
+++ hardware/src/iob_eth_dma.v
@@ -38,11 +38,47 @@ module iob_eth_dma #(
    // AXI master interface
    `include "axi_m_port.vs"
 
+   // Interrupts
+   output tx_irq_o,
+   output rx_irq_o,
+
    input clk_i,
    input cke_i,
    input arst_i
 );
    // ############# Transmitter #############
+
+   wire [1:0] bd_mem_arbiter_req;
+   wire [1:0] bd_mem_arbiter_ack;
+   wire [1:0] bd_mem_arbiter_grant;
+   wire [1:0] bd_mem_arbiter_grant_valid;
+   wire [$clog2(PORTS)-1:0] bd_mem_arbiter_grant_encoded;
+   module arbiter #(
+      .PORTS(2),
+      // arbitration type: "PRIORITY" or "ROUND_ROBIN"
+      .TYPE("PRIORITY"),
+      // block type: "NONE", "REQUEST", "ACKNOWLEDGE"
+      .BLOCK("NONE"),
+      // LSB priority: "LOW", "HIGH"
+      .LSB_PRIORITY("LOW")
+   ) bd_mem_arbiter (
+      .clk(clk_i),
+      .rst(arst_i),
+
+      .request(bd_mem_arbiter_req),
+      .acknowledge(bd_mem_arbiter_ack),
+
+      .grant(bd_mem_arbiter_grant),
+      .grant_valid(bd_mem_arbiter_grant_valid),
+      .grant_encoded(bd_mem_arbiter_grant_encoded)
+   );
+
+   // Connect BD memory bus based on arbiter selection
+   assign bd_addr_o = bd_mem_arbiter_grant_encoded==0 ? tx_bd_addr_o : rx_bd_addr_o;
+   assign bd_wen_o = bd_mem_arbiter_grant_encoded==0 ? tx_bd_wen_o : rx_bd_wen_o;
+   assign bd_o = bd_mem_arbiter_grant_encoded==0 ? tx_bd_o : rx_bd_o;
+
+   assign bd_en_o = 1'b1;
 
    //tx program
    reg [1:0] tx_pc;
@@ -53,10 +89,9 @@ module iob_eth_dma #(
 
          tx_pc       <= 1'b0;
          tx_bd_num   <= 1'b0;
-         bd_en_o     <= 1'b1;
-         bd_addr_o   <= 1'b0;
-         bd_wen_o    <= 1'b0;
-         bd_o        <= 1'b0;
+         tx_bd_addr_o   <= 1'b0;
+         tx_bd_wen_o    <= 1'b0;
+         tx_bd_o        <= 1'b0;
 
       end else if (tx_en_i) begin
 
@@ -65,36 +100,40 @@ module iob_eth_dma #(
          case (tx_pc)
 
             0: begin  // Read buffer descriptor
-               bd_addr_o <= tx_bd_num<<1;
+               tx_bd_addr_o <= tx_bd_num<<1;
             end
 
             1: begin  // Read buffer pointer.
                buffer_descriptor <= bd_i;
-               bd_addr_o <= tx_bd_num<<1 + 1;
+               tx_bd_addr_o <= tx_bd_num<<1 + 1;
 
-               // Wait for ready bit
-               if (bd_i[15]==0)
-                  bd_addr_o <= tx_bd_num<<1;
+               // Wait for ready bit and
+               // wait for arbiter and
+               // wait for buffer ready for next frame
+               if (bd_i[15]==0) ||
+                  (bd_mem_arbiter_ack[0]==0 || bd_mem_arbiter_grant[0]==0 || bd_mem_arbiter_grant_valid==0)
+                  // TODO: Check buffer ready for next frame
+                  tx_bd_addr_o <= tx_bd_num<<1;
                   tx_pc <= tx_pc;
             end
 
-            2: begin  // Store buffer pointer; Read frame from external memeory.
+            2: begin  // Store buffer pointer
                buffer_ptr <= bd_i;
-               buffer_word_counter <= 1;
-
-               //TODO: Read frame word from extmem; Send word to FIFO
-               axi_addr <= bd_i;
+               buffer_word_counter <= 0;
             end
 
-            3: begin  // Read next word from buffer;
-               tx_pc <= tx_pc;
-               buffer_word_counter <= buffer_word_counter + 1'b1;
+            3: begin  // Start frame transfer from external memory
+               axi_araddr_o <= buffer_ptr + buffer_word_counter;
+               axi_arlen_o <= `IOB_MIN(16,buffer_descriptor[31:16]-buffer_word_counter);
+               axi_arvalid_o <= 1'b1;
+               // Wait for address ready
+               if (axi_arready_i==0)
+                  tx_pc <= tx_pc;
 
-               //TODO: Read frame word from extmem; Send word to FIFO
-               axi_addr <= buffer_ptr + buffer_word_counter;
+               // Check if frame transfer is complete
+               if (buffer_descriptor[31:16]-buffer_word_counter == 0)
+                  axi_arvalid_o <= 1'b0;
 
-               // Finished transmission
-               if (buffer_word_counter == buffer_descriptor[31:16]) begin
                   // Reset buffer word counter and go to next buffer descriptor
                   buffer_word_counter <= 1'b0;
                   tx_pc <= 1'b0;
@@ -103,14 +142,30 @@ module iob_eth_dma #(
                   // - Disable ready bit
 
                   // Generate interrupt
+                  assign tx_irq_o = 1'b1;
 
                   // Select BD address based on WR bit
                   if (buffer_descriptor[13] == 0)
                      tx_bd_num <= tx_bd_num + 1'b1;
                   else
                      tx_bd_num <= 1'b0
+            end
 
-               end
+            4: begin // receive frame word
+               tx_pc <= tx_pc;
+
+               if (axi_rvalid_i==1)
+                  buffer_word_counter <= buffer_word_counter + 1'b1;
+                  axi_rready_o <= 1'b1;
+                  // Send word to buffer
+                  eth_data_wr_wen_o <= 1'b1;
+                  eth_data_wr_wstrb_o <= 4'hf;
+                  eth_data_wr_addr_o <= buffer_word_counter;
+                  eth_data_wr_wdata_o <= axi_rdata_i;
+
+                  if (axi_rlast_i==1)
+                     tx_pc <= 3;
+
             end
 
             default: ;
@@ -120,24 +175,23 @@ module iob_eth_dma #(
       end else begin
 
          tx_pc       <= 1'b0;
-         bd_en_o     <= 1'b1;
-         bd_addr_o   <= BD_ADDR_W'h0;
-         bd_wen_o    <= 1'b0;
-         bd_o        <= 1'b0;
+         tx_bd_addr_o   <= BD_ADDR_W'h0;
+         tx_bd_wen_o    <= 1'b0;
+         tx_bd_o        <= 1'b0;
 
       end
 
-   // AXI Master Write interface
+   // AXI Master Read interface
    // Constants
-   assign axi_awid_o    = 0;
-   assign axi_awsize_o  = 2;
-   assign axi_awburst_o = 1;
-   assign axi_awlock_o  = 0;
-   assign axi_awcache_o = 2;
-   assign axi_awprot_o  = 2;
-   assign axi_awqos_o   = 0;
-   assign axi_wstrb_o   = 4'b1111;
-   assign axi_bready_o  = 1'b1;
+   assign axi_arid_o    = 0;
+   assign axi_arsize_o  = 2;
+   assign axi_arburst_o = 1;
+   assign axi_arlock_o  = 0;
+   assign axi_arcache_o = 2;
+   assign axi_arprot_o  = 2;
+   assign axi_arqos_o   = 0;
+   //axi_rid_i
+   //axi_rresp_i
 
    //TODO
 
@@ -152,10 +206,9 @@ module iob_eth_dma #(
 
          rx_pc       <= 1'b0;
          rx_bd_num   <= 1'b0;
-         bd_en_o     <= 1'b1;
-         bd_addr_o   <= 1'b0;
-         bd_wen_o    <= 1'b0;
-         bd_o        <= 1'b0;
+         rx_bd_addr_o   <= 1'b0;
+         rx_bd_wen_o    <= 1'b0;
+         rx_bd_o        <= 1'b0;
 
       end else if (rx_en_i) begin
 
@@ -164,16 +217,18 @@ module iob_eth_dma #(
          case (rx_pc)
 
             0: begin  // Read buffer descriptor
-               bd_addr_o <= rx_bd_num<<1;
+               rx_bd_addr_o <= rx_bd_num<<1;
             end
 
             1: begin  // Read buffer pointer.
                buffer_descriptor <= bd_i;
-               bd_addr_o <= rx_bd_num<<1 + 1;
+               rx_bd_addr_o <= rx_bd_num<<1 + 1;
 
-               // Wait for empty bit
-               if (bd_i[15]==0)
-                  bd_addr_o <= rx_bd_num<<1;
+               // Wait for ready bit and
+               // wait for arbiter
+               if (bd_i[15]==0) ||
+                  (bd_mem_arbiter_ack[1]==0 || bd_mem_arbiter_grant[1]==0 || bd_mem_arbiter_grant_valid==0)
+                  rx_bd_addr_o <= rx_bd_num<<1;
                   rx_pc <= rx_pc;
             end
 
@@ -181,7 +236,7 @@ module iob_eth_dma #(
                buffer_ptr <= bd_i;
                buffer_word_counter <= 1;
 
-               //TODO: Write frame word to extmem; Get word from FIFO
+               //TODO: Write frame word to extmem; Get word from buffer
                axi_addr <= bd_i;
             end
 
@@ -189,7 +244,7 @@ module iob_eth_dma #(
                rx_pc <= rx_pc;
                buffer_word_counter <= buffer_word_counter + 1'b1;
 
-               //TODO: Write frame word to extmem; Send word to FIFO
+               //TODO: Write frame word to extmem; Send word to buffer
                axi_addr <= buffer_ptr + buffer_word_counter;
 
                // Finished transmission
@@ -219,14 +274,23 @@ module iob_eth_dma #(
       end else begin
 
          tx_pc       <= 1'b0;
-         bd_en_o     <= 1'b1;
-         bd_addr_o   <= BD_ADDR_W'h0;
-         bd_wen_o    <= 1'b0;
-         bd_o        <= 1'b0;
+         rx_bd_addr_o   <= BD_ADDR_W'h0;
+         rx_bd_wen_o    <= 1'b0;
+         rx_bd_o        <= 1'b0;
 
       end
 
-   // AXI Master Read interface
+   // AXI Master Write interface
+   // Constants
+   assign axi_awid_o    = 0;
+   assign axi_awsize_o  = 2;
+   assign axi_awburst_o = 1;
+   assign axi_awlock_o  = 0;
+   assign axi_awcache_o = 2;
+   assign axi_awprot_o  = 2;
+   assign axi_awqos_o   = 0;
+   assign axi_wstrb_o   = 4'b1111;
+   assign axi_bready_o  = 1'b1;
 
 
 
