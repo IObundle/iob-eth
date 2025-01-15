8da37f2a41298f7020f10812701cdac8009f07af
Andre Nobrega <29956896+And-Nob@users.noreply.github.com>
2025-01-15 01:02:28 +0000

    Formats code

--- scripts/pyRawWrapper/pyRawWrapper.c
+++ scripts/pyRawWrapper/pyRawWrapper.c
@@ -1,27 +1,28 @@
 /*
-* Python wrapper for CAP_NET_RAW capability
-* Based on solution from here: https://stackoverflow.com/a/67733220/11442904
-*/
+ * Python wrapper for CAP_NET_RAW capability
+ * Based on solution from here: https://stackoverflow.com/a/67733220/11442904
+ */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/prctl.h>
 #include <sys/capability.h>
+#include <sys/prctl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    cap_iab_t iab = cap_iab_from_text("^cap_net_raw");
-    if (iab == NULL) {
-        perror("iab not parsed");
-        exit(1);
-    }
-    if (cap_iab_set_proc(iab)) {
-        perror("unable to set iab");
-        exit(1);
-    }
-    cap_free(iab);
+  cap_iab_t iab = cap_iab_from_text("^cap_net_raw");
+  if (iab == NULL) {
+    perror("iab not parsed");
+    exit(1);
+  }
+  if (cap_iab_set_proc(iab)) {
+    perror("unable to set iab");
+    exit(1);
+  }
+  cap_free(iab);
 
-    // CAP_NET_RAW capability is already set, proceed to execute 'python' with provided arguments
-    execvp("python", argv);
-    perror("execvp");
-    return 1;
+  // CAP_NET_RAW capability is already set, proceed to execute 'python' with
+  // provided arguments
+  execvp("python", argv);
+  perror("execvp");
+  return 1;
 }
