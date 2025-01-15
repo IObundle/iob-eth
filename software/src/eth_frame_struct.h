94953287eedb9be462b8e25903289fc43319342c
Andre Nobrega <29956896+And-Nob@users.noreply.github.com>
2025-01-15 01:04:36 +0000

    Formats code

--- software/src/eth_frame_struct.h
+++ software/src/eth_frame_struct.h
@@ -2,6 +2,6 @@
 #define ETH_TYPE_L 0x00
 
 #define ETH_NBYTES 1500
-#define ETH_MINIMUM_NBYTES (64-18)
+#define ETH_MINIMUM_NBYTES (64 - 18)
 
-#define HDR_LEN      (2*IOB_ETH_MAC_ADDR_LEN + 2)
+#define HDR_LEN (2 * IOB_ETH_MAC_ADDR_LEN + 2)
