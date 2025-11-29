ef5c9f55a034e4d87f065b40ea71f0a53e2ec062
P-Miranda <pmiranda.iobundle@gmail.com>
2025-11-29 20:05:42 +0000

    feat(py2): update py2 commit - update with lint fixes

--- default.nix
+++ default.nix
@@ -5,8 +5,8 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/25.05.tar.gz") {} }:
 
 let
-  py2hwsw_commit = "063b0c34ddbcdf82ba9582ad50a5ae15a3827be0"; # Replace with the desired commit.
-  py2hwsw_sha256 = "xq2u3HTkcpthoLWKqjmsjCqUZWFn+07LpEfutTYx9KU="; # Replace with the actual SHA256 hash.
+  py2hwsw_commit = "d8426b71be7eae8bc77d95d44cd80e0a2ce70700"; # Replace with the desired commit.
+  py2hwsw_sha256 = "DSk4SlEEEXuQnJWELOjiDnBslrSw/odT4lhYTjBEhAo="; # Replace with the actual SHA256 hash.
   # Get local py2hwsw root from `PY2HWSW_ROOT` env variable
   py2hwswRoot = builtins.getEnv "PY2HWSW_ROOT";
 
