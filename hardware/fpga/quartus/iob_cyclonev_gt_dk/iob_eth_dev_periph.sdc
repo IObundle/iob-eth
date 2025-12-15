2c403103995bc05452c1570eb434b79ea69b879a
P-Miranda <pmiranda.iobundle@gmail.com>
2025-12-15 20:26:51 +0000

    feat(cyclonev): add sdc for CYCLONEV-DT-DK board

---
+++ hardware/fpga/quartus/iob_cyclonev_gt_dk/iob_eth_dev_periph.sdc
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2025 IObundle
+#
+# SPDX-License-Identifier: MIT
+
+# ----------------------------------------------------------------------------
+# IOb_Eth Example Constrain File
+# SPDX-FileCopyrightText: 2025 IObundle
+#
+# This file contains the ethernet core constraints for the CYCLONEV-DT-DK board.
+# ----------------------------------------------------------------------------
+
+#Constraint Clock Transitions
+# RX_CLK is 25MHz for 100Mbps operation according to Texas Instruments DP83867 datasheet
+create_clock -name "rx_eth_clk" -period 40 [get_ports {enet_rx_clk_i}]
--- iob_eth.py
+++ iob_eth.py
@@ -46,13 +46,17 @@ def setup(py_params_dict):
     if py_params_dict.get("py2hwsw_target", "") == "setup":
         # check if eth is top module
         if py_params_dict["issuer"]:
-            ku040_sdc = "iob_eth_dev_periph.sdc"
+            dev_sdc = "iob_eth_dev_periph.sdc"
         else:
-            ku040_sdc = "iob_eth_dev_top.sdc"
+            dev_sdc = "iob_eth_dev_top.sdc"
         paths = [
             (
-                f"hardware/fpga/vivado/iob_aes_ku040_db_g/{ku040_sdc}",
+                f"hardware/fpga/vivado/iob_aes_ku040_db_g/{dev_sdc}",
                 "hardware/fpga/vivado/iob_aes_ku040_db_g/iob_eth_dev.sdc",
+            ),
+            (
+                f"hardware/fpga/quartus/iob_cyclonev_gt_dk/{dev_sdc}",
+                "hardware/fpga/quartus/iob_cyclonev_gt_dk/iob_eth_dev.sdc",
             ),
         ]
 
